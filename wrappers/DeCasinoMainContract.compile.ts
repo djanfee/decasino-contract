1d07a3e12c9871f76617ad280a17180c625087f7
youbing <djanfee@gmail.com>
2024-12-30 19:55:54 +0700

    -feat update

--- wrappers/DeCasinoMainContract.compile.ts
+++ wrappers/DeCasinoMainContract.compile.ts
@@ -2,7 +2,7 @@ import { CompilerConfig } from '@ton/blueprint';
 
 export const compile: CompilerConfig = {
     lang: 'tact',
-    target: 'contracts/de_casino_main_contract.tact',
+    target: 'contracts/de_casino_main.tact',
     options: {
         debug: true,
     },
