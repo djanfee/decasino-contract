97534e1c6f8dd27ca82192962647cfb2fb2d6f47
youbing <djanfee@gmail.com>
2025-01-17 19:57:29 +0700

    -feat update

--- scripts/deployDeCasinoMainContract.ts
+++ scripts/deployDeCasinoMainContract.ts
@@ -1,9 +1,16 @@
-import { toNano } from '@ton/core';
+import { toNano,Cell } from '@ton/core';
 import { DeCasinoMainContract } from '../wrappers/DeCasinoMainContract';
 import { NetworkProvider } from '@ton/blueprint';
+import {DecasinoMainContractStateInit} from '../build/DeCasinoMainContract/tact_DeCasinoMainContract';
+import fs from 'fs';
 
 export async function run(provider: NetworkProvider) {
-    const deCasinoMainContract = provider.open(await DeCasinoMainContract.fromInit(BigInt(Math.floor(Math.random() * 10000))));
+    let initData:DecasinoMainContractStateInit = {
+        $$type: 'DecasinoMainContractStateInit',
+        walletCode: Cell.fromBoc(Buffer.from(await fs.readFileSync('./build/DeCasinoMainContract/tact_DeCasinoMainContract.code.fc')))[0],
+        revenueCode: Cell.fromBoc(Buffer.from(await fs.readFileSync('./build/DeCasinoMainContract/tact_DeCasinoMainContract.code.fc')))[0],
+    };
+    const deCasinoMainContract = provider.open(await DeCasinoMainContract.fromInit(initData));
 
     await deCasinoMainContract.send(
         provider.sender(),
